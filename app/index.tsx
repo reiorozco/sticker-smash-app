282adfddf53a8f52a785b0a6bb3a2f71f34f2193
reiorozco <rfoc15@gmail.com>
2025-04-30 15:47:47 -0500

    Add navigation and new screens for NotFound and About

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+
+      <Link href="/about" style={styles.button}>
+        Go to about screen
+      </Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    color: "#fff",
+    fontSize: 20,
+    textDecorationLine: "underline",
+  },
   container: {
     alignItems: "center",
     backgroundColor: "#25292e",
